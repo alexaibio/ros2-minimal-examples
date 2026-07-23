88f3d01ba3bb1315d2c3f3ac9db582129bc988c9
alexaibio <github.alex.ai@gmail.com>
2026-07-23 12:41:04 +0300

    Polish client and server

--- ch2/workspace/src/srvcli_py/srvcli_py/client.py
+++ ch2/workspace/src/srvcli_py/srvcli_py/client.py
@@ -1,8 +1,8 @@
 import sys
 import rclpy
 from rclpy.node import Node
-# Import the service message type used to fill the request
-from srv_pkg.srv import JointConversion
+
+from srv_pkg.srv import JointConversion     # Import the service message type used to fill the request
 
 
 class JointConversionClient(Node):
@@ -10,24 +10,28 @@ class JointConversionClient(Node):
     def __init__(self):
         super().__init__('joint_conversion_client')
 
-	# Create the service client object and wait that it appears online
-        self.cli = self.create_client(JointConversion, 'joint_conversion')
-        while not self.cli.wait_for_service(timeout_sec=1.0):
+	    # Create the service client object and wait that it appears online
+        self.cli = self.create_client(
+            srv_type=JointConversion, 
+            srv_name='joint_conversion')
+
+        while not self.cli.wait_for_service(timeout_sec=3.0):
             self.get_logger().info('service not available, waiting again...')
 
 
-    def send_request(self, j_values):
+    def send_request(self, j_values: list):
     
         # Fill the request part, considering the 7 joint values    
         req = JointConversion.Request()
-        for i in range( len(j_values) ):
-           req.joint_input[i].data = j_values[i]
+        for joint_msg, value in zip(req.joint_input, j_values):
+            joint_msg.data = value
 
-        # Call the server, using the client object
-        # The result is a future object that can be used to monitor its status
+        # Call the server: The result is a future object that can be used to monitor its status
+        # note: it returns immediately (async) - returna a future (result will be available later)
         self.future = self.cli.call_async(req)
         
-        # The future object is used in this function to wait that the server replies  with a given resultt
+        # wait that the server replies
+        # in general cae we can do /while not future.done():/ and not to block execution, here is just an example
         rclpy.spin_until_future_complete(self, self.future)
         
         # The result is returned to the main function
@@ -40,12 +44,12 @@ def main(args=None):
 
     conversion_client = JointConversionClient()
     
-    # Set random values for the joint vector
     joint_values = [1.0, 0.78, 1.6, 0.0, 1.57, 0.3, 1.2]
   
-    # Call the client
     response = conversion_client.send_request(joint_values)
-    print("Response: ", response)
+    print("Joint conversion results:")
+    for i, (inp, out) in enumerate(zip(joint_values, response.joint_output), start=1):
+        print(f"  Joint {i}: {inp:.2f} rad  ->  {out.data:.2f}°")
 
     conversion_client.destroy_node()
     rclpy.shutdown()
--- ch2/workspace/src/srvcli_py/srvcli_py/service.py
+++ ch2/workspace/src/srvcli_py/srvcli_py/service.py
@@ -4,13 +4,17 @@ from srv_pkg.srv import JointConversion
 from rclpy.node import Node
 
 
+# Node class
 class JointConversionService(Node):
 
     def __init__(self):
-        super().__init__('joint_conversion_service')
+        super().__init__('joint_conversion_service')    # Name of the Node
 	
-	# Create the service object. Here we specify the function that is invoked when the service is called
-        self.srv = self.create_service(JointConversion, 'joint_conversion', self.joint_conversion_callback)
+	    # Create the service object. Here we specify the function that is invoked when the service is called
+        self.srv = self.create_service(
+            srv_type=JointConversion,       # interface, defined in srv_pkg
+            srv_name='joint_conversion', 
+            callback=self.joint_conversion_callback)
         
         # A random set of values representing the offset to add to the joint vector
         self.offset = [0.2, 0.2, 0.2, 0.5, 0.5, 0.5, 0.0]
@@ -18,9 +22,7 @@ class JointConversionService(Node):
     def joint_conversion_callback(self, request, response):
         
         # Save the input vector of joints into a list
-        j_values = []
-        for i in range( len(request.joint_input) ):
-           j_values.append(request.joint_input[i].data)
+        j_values = [joint.data for joint in request.joint_input]
                    
         # Apply the offset
         result = [x + y for x, y in zip(j_values, self.offset)]
@@ -28,12 +30,12 @@ class JointConversionService(Node):
         # Transform from rad to degree
         result = [(x * 180.0) / 3.1415 for x in result]
 
-	# Retro transofrm the list into the result field of the service
-        for i in range( len(result) ):
-           response.joint_output[i].data = result[i]
+	    # Retro transofrm the list into the result field of the service
+        for out_joint, value in zip(response.joint_output, result):
+            out_joint.data = value
 
         # Just wait a bit, to check the output on the terminal
-        time.sleep(2)
+        time.sleep(5)
         print("Conversion performed")
         return response
 
